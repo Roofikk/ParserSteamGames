43e3666465cc870bbea931a88107d981262c1332
Андрей Руфов <rufik956@gmail.com>
2024-02-20 17:24:34 +0300

    Добавил еще немного функциональности и убрал баги

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import aiohttp
 import json
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+import os
+from datetime import datetime
+import uuid
+
+
+current_dir = os.path.join(os.getcwd(), 'outputs')
+now = datetime.now().strftime('%d-%m-%Y %H-%M-%S')
+path_dir_games = os.path.join(current_dir, now, 'successful')
+path_dir_failed_games = os.path.join(current_dir, now, 'failed')
+
+os.makedirs(path_dir_games)
+os.makedirs(path_dir_failed_games)
+
 
 def get_all_games():
     games_url = "https://api.steampowered.com/ISteamApps/GetAppList/v2?format=json"
@@ -15,7 +28,7 @@ def get_all_games():
 def format_game_data(game_id, response):
     if response is None:
         # print(f"Couldn't get game info by id: {game_id}")
-        return {game_id: {'reason': 'unknown'}}
+        return {game_id: {'success': False, 'reason': 'unknown'}}
 
     # print(f"Getting app by id: {game_id}")
 
@@ -23,19 +36,19 @@ def format_game_data(game_id, response):
 
     if "success" in json.keys():
         if json["success"] == False:
-            return {game_id: {'reason': json.get('reason', 'unknown')}}
+            return {game_id: {'success': False, 'reason': json.get('reason', 'unknown')}}
 
     game_data = dict(json.get('data'))
 
     # Проверка на тип приложения
     if game_data["type"] != "game":
-        return {game_id: {'reason': 'is not game'}}
+        return {game_id: {'success': False, 'reason': 'is not game'}}
 
     # также нужна проверка на релизную игру
 
     if 'coming_soon' in game_data['release_date'].keys():
         if game_data['release_date']['coming_soon'] == True:
-            return {game_id: {'reason': 'not released'}}
+            return {game_id: {'success': False, 'reason': 'not released'}}
 
     my_data = {game_id: {
         "success": True,
@@ -108,37 +121,93 @@ async def get_game_data(game_id: str):
         return {game_id: {'success': False, 'reason': 'timeout error'}}
 
 
-async def get_games_info(games: list):
+# оптимальное количество запросов в секунду = 0,75 req/s
+# если я правильно понял
+async def write_games_info(games: list, count_game_to_write_file: int):
     tasks = []
-    games_format_data = []
+    games_format_data_dict = {}
+    failed_games_dict = {}
 
     for game in games:
-        id = str(game["appid"])
-        tasks.append(get_game_data(id))
+        game_id = str(game["appid"])
+        tasks.append(get_game_data(game_id))
 
-    # оптимальное количество запросов в секунду = 0,75 req/s
-    # если я правильно понял
-    counter = 0
+    game_counter = 0
+    task_counter = 0
     running_tasks = []
+
     for task in tqdm(tasks):
         running_tasks.append(task)
-        counter += 1
+        task_counter += 1
+        game_counter += 1
 
-        if counter >= 4:
-            games_format_data.extend(await asyncio.gather(*running_tasks, return_exceptions=True))
+        if task_counter >= 4:
+            response_games = await asyncio.gather(*running_tasks, return_exceptions=True)
+
+            games_dict = {}
+            for g in response_games:
+                games_dict.update(g)
+
+            failed_games = {game_id: games_dict[game_id] for game_id in games_dict if games_dict[game_id]['success'] == False}
+            failed_games_dict.update(failed_games)
+
+            success_games = {game_id: games_dict[game_id] for game_id in games_dict if games_dict[game_id]['success'] == True}
+            games_format_data_dict.update(success_games)
+
             await asyncio.sleep(5.6)
             running_tasks.clear()
-            counter = 0
+            task_counter = 0
 
-    return games_format_data
+        if game_counter >= count_game_to_write_file:
+            write_json_file(games_format_data_dict, os.path.join(path_dir_games, str(uuid.uuid4()) + '.json'))
+            games_format_data_dict.clear()
+
+            write_json_file(failed_games_dict, os.path.join(path_dir_failed_games, str(uuid.uuid4()) + '.json'))
+            failed_games_dict.clear()
+
+            game_counter = 0
+
+async def try_again_get_games(path: str, games: list):
+    again_failed = []
+    success_games = []
+
+    for row_game in games:
+        format_game = await get_game_data(row_game)
+        await asyncio.sleep(0.7)
+
+        if format_game['success'] is False:
+            again_failed.append(format_game)
+        else:
+            success_games.append(format_game)
+
+    return {'successes': success_games, 'failed': again_failed}
 
 
-if __name__ == "__main__":
-    games = get_all_games()
+def write_json_file(games_list: dict, path_file: str):
+    with open(path_file, 'w', encoding='utf-8') as f:
+        json.dump(games_list, f, ensure_ascii=False, indent=4)
+        f.close()
+
+def get_json_file(path_file: str):
+    f = open(path_file, 'r', encoding='utf-8')
+    json_data = json.load(f)
+    f.close()
+
+    return json_data
+
+
+async def main(path_file=''):
+    if path_file is None or path_file == '':
+        games = get_all_games()
+        write_json_file(games, os.path.join(current_dir, 'games.json'))
+    else:
+        get_json_file(os.path.join(current_dir, 'games.json'))
+
     non_empty_games = [game for game in games if game['name'] != '']
     print(f"Games amount: {len(non_empty_games)}")
 
-    loop = asyncio.new_event_loop()
-    loop.set_debug(True)
-    result = loop.run_until_complete(get_games_info(non_empty_games))
-    loop.close()
+    await write_games_info(non_empty_games, 100)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
